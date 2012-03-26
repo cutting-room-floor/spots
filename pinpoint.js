a4c58a8eeb920305c07e7f94fd9b3d088d619fc6
Michael Aufreiter <ma@zive.at>
2012-03-26 10:22:33 -0400

    Use a string literal for the template. No longer use markup for the description.

--- pinpoint.js
+++ pinpoint.js
@@ -2,6 +2,15 @@
 // Pinpoint.js is freely distributable under the BSD license.
 
 (function(window){
+  
+
+  var TEMPLATE = '\
+        <div class="pin" id="<%= id%>"> \
+        <div class="name<%= !name ? " empty" : "" %>" title="Click to edit"><%= !name ? "Enter Name" : name %></div> \
+        <div class="controls" style="height: 20px; margin-top: 10px;"></div> \
+        <div class="descr<%= !descr ? " empty" : "" %>" title="Click to edit"><%= !descr ? "<p>Description&hellip;</p>" : descr %></div> \
+        <div><a href="#" class="remove-pin">Remove Pin</div> \
+      </div>';
 
   // Initial Setup
   // -------------
@@ -12,7 +21,7 @@
     this.editor = new Proper();
 
     function tpl(name, data) {
-      return _.template($('script[name='+name+']').html(), data);
+      return _.template(TEMPLATE, data);
     }
 
     function initPin(pin) {
@@ -21,11 +30,11 @@
       if (pin.name) $el.find('.name').html(pin.name);
       if (pin.descr) $el.find('.descr').html(pin.descr);
 
-
       function init() {
         // Delete pin handler`
         $el.find('.remove-pin').unbind().bind('click', function() {
-          return that.removePin(pin.id);
+          that.removePin(pin.id);
+          return false;
         });
 
         // Editor for pin name
@@ -40,7 +49,7 @@
 
           that.editor.bind('changed', function() {
             pin.name = that.editor.content();
-            options.update(that.pins);
+            options.update(that.getPins());
           });
 
            $el.find('.controls').removeClass('activated');
@@ -52,15 +61,14 @@
         that.$descr.click(function() {
           that.editor.activate(that.$descr, {
             placeholder: '<p>Enter Description&hellip;</p>',
-            controlsTarget: $el.find('.controls')
+            controlsTarget: $el.find('.controls'),
+            markup: false
           });
 
           that.editor.bind('changed', function() {
             pin.descr = that.editor.content();
-            options.update(that.pins);
+            options.update(that.getPins());
           });
-
-          $el.find('.controls').addClass('activated');
         });
       }
 
@@ -84,33 +92,59 @@
       initPin(that.pins[$(e.popup._container).find('.pin').attr('id')]);
     });
 
+
     // Expose public API
     // -------------
 
-    this.addPin = function(lat, lng, name, descr) {
+    this.addPin = function(lat, lng, name, descr, id, silent) {
       var pin = {
-        id: map._container.id + _.uniqueId('_pin_'),
+        id: id ? id : map._container.id + _.uniqueId('_pin_'),
         name: name,
-        descr: descr
+        descr: descr,
+        latitude: lat,
+        longitude: lng
       };
 
       that.pins[pin.id] = pin;
       pin.marker = new L.Marker(new L.LatLng(lat, lng), { draggable: true });
+
+      // Update when dragged.
+      pin.marker.on('drag', function(e) {
+        var pos = e.target._latlng;
+        pin.latitude = pos.lat;
+        pin.longitude = pos.lng;
+        options.update(that.getPins());
+      });
+
       map.addLayer(pin.marker);
       pin.marker.bindPopup(tpl('pinpoint_annotation', pin)).openPopup();
-      options.update(that.pins);
+      if (!silent) options.update(that.getPins());
     }
 
     this.removePin = function(pin) {
       var pin = this.pins[pin];
       map.removeLayer(pin.marker);
       delete that.pins[pin.id];
-      options.update(that.pins);
+      options.update(that.getPins());
     };
 
     this.getPins = function() {
-      return that.pins;
+      var pins = {};
+      _.each(that.pins, function(p, key) {
+        pins[key] = {
+          name: p.name,
+          descr: p.descr,
+          latitude: p.latitude,
+          longitude: p.longitude
+        };
+      });
+      return pins;
     }
+
+    // Restore saved pins
+    _.each(options.pins, function(p, id) {
+      that.addPin(p.latitude, p.longitude, p.name, p.descr, id, true);
+    });
   };
 
 })(window);
