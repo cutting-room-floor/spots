1705b4616059300ac0b83801303a25d3200a90c1
Tom MacWright <tom@macwright.org>
2012-04-12 12:12:05 -0400

    Rewrite mmg

--- site.js
+++ site.js
@@ -2,5 +2,18 @@ $(function() {
   wax.tilejson('http://a.tiles.mapbox.com/v3/mapbox.mapbox-streets.jsonp', function(tj) {
     var map = new MM.Map('map', wax.mm.connector(tj));
     map.zoom(5).center({ lat: 37, lon: -77 });
+
+    var geojson = {
+      type: "FeatureCollection",
+      features: []
+    };
+
+    var markers = mmg().geojson(geojson).map(map);
+    console.log(markers);
+    map.addLayer(markers);
+
+    $('#add-point').click(function() {
+        markers.dropper();
+    });
   });
 });
